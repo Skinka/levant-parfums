3107369f6f05352909e3fb365eeabcaf5367b42c
Skinka <skinka83@gmail.com>
2026-05-22 16:51:58 +0300

    catalogue: add catalogue config (default volume, currency map)

---
+++ config/catalogue.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'default_volume_ml' => 50,
+
+    'currency_by_locale' => [
+        'uk' => 'UAH',
+        'en' => 'EUR',
+    ],
+
+    'locales' => ['uk', 'en'],
+    'default_locale' => 'uk',
+];
