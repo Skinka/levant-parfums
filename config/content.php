2adc0eccd16152a99ad06ba2630d29093298d7bb
Skinka <skinka83@gmail.com>
2026-05-23 08:54:58 +0300

    content: add reserved_slugs config

---
+++ config/content.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reserved_slugs' => [
+        'admin', 'api', 'assets', 'storage', 'login', 'register', 'logout',
+        'blog', 'articles', 'pages', 'sitemap', 'feed',
+        'uk', 'en',
+    ],
+];
