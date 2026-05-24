73d58083aa0e513b44c91f682a0a43c5928d55a7
Skinka <skinka83@gmail.com>
2026-05-24 23:40:36 +0300

    feat(homepage): extend HeroBlock with editorial split fields

--- app/Filament/Resources/Pages/Schemas/Blocks/HeroBlock.php
+++ app/Filament/Resources/Pages/Schemas/Blocks/HeroBlock.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Pages\Schemas\Blocks;
 use App\Filament\Resources\Pages\Schemas\Blocks\Concerns\TranslatableTabs;
 use Filament\Forms\Components\Builder\Block;
 use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
@@ -18,13 +19,22 @@ class HeroBlock
             ->icon('heroicon-o-rectangle-stack')
             ->schema([
                 ...self::commonFields(),
-                TranslatableTabs::make('title', required: true),
-                TranslatableTabs::make('subtitle', component: Textarea::class),
+                TranslatableTabs::make('eyebrow'),
+                TranslatableTabs::make('title_top', required: true),
+                TranslatableTabs::make('title_bottom'),
+                TranslatableTabs::make('lead', component: Textarea::class),
+                TranslatableTabs::make('floating_label'),
+
                 TranslatableTabs::make('cta_label'),
                 TextInput::make('cta_url')
                     ->label(trans('content.blocks.fields.cta_url'))
                     ->maxLength(2048)
                     ->helperText(trans('content.blocks.fields.cta_url_helper')),
+                TranslatableTabs::make('secondary_cta_label'),
+                TextInput::make('secondary_cta_url')
+                    ->label(trans('content.blocks.fields.secondary_cta_url'))
+                    ->maxLength(2048),
+
                 FileUpload::make('image_path')
                     ->label(trans('content.blocks.fields.image_path'))
                     ->disk('public')
@@ -32,6 +42,20 @@ class HeroBlock
                     ->image()
                     ->imageEditor()
                     ->maxSize(4096),
+
+                Repeater::make('meta')
+                    ->label(trans('content.blocks.fields.meta'))
+                    ->schema([
+                        TextInput::make('num')
+                            ->label(trans('content.blocks.fields.meta_num'))
+                            ->required()
+                            ->maxLength(8),
+                        TranslatableTabs::make('meta_label', required: true),
+                    ])
+                    ->minItems(3)
+                    ->maxItems(3)
+                    ->defaultItems(3)
+                    ->reorderable(),
             ]);
     }
 
