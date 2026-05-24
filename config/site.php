0306df194abd8fd271e586c50cfbef47c207697a
Skinka <skinka83@gmail.com>
2026-05-24 14:28:42 +0300

    site: theme config + layout reads $theme prop

---
+++ config/site.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'themes' => [
+        'theme-cream' => 'Cream (Luxury)',
+        'theme-onyx' => 'Onyx (Dark)',
+        'theme-editorial' => 'Editorial (Minimal)',
+    ],
+];
